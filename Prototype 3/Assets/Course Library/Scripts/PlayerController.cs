cf9fa1bba82f493a64d283920f3867370f9b596a
luigitheartist <barbosaj@roverkids.org>
2024-12-12 13:46:22 -0500

    Done with 3.1

--- Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody pRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround=true;    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,11 +17,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             pRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
 
     }
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
 }
