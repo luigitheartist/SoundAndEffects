d9ceff9ccd1d8bcf378dcb9a1c188911fff79233
luigitheartist <barbosaj@roverkids.org>
2024-12-12 14:17:34 -0500

    50% done with 3.2

--- Prototype 3/Assets/Course Library/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Course Library/Scripts/MoveLeft.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30;
+    private float speed = 15;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -26,6 +26,13 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over :(");
+        }
     }
 }
